cb530344b2b242085f31deff3d612ecd3fb24844
Emmanuel <emmanuel@Emmanuels-MacBook-Pro.local>
2019-02-15 09:18:09 +0800

    Rendered using reactstrap media

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Navbar, NavbarBrand}from 'reactstrap';
+
+//COMPONENTS
+import Menu from './components/MenuComponent';
+import {DISHES} from './components/shared/dishes';
+
+
+
 
 class App extends Component {
+  state = {
+    dishes: DISHES
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+         <Navbar dark color = "danger">
+            <div className ="container">
+             <NavbarBrand href = "/">
+                 Bite your Tongue
+             </NavbarBrand>
+            </div>
+         </Navbar>
+
+         <Menu dishes ={this.state.dishes}/>
       </div>
     );
   }
